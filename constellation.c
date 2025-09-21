bda6e9efba9928bc4e94afb9eb0f134fd2aeafe0
_danilka_ <danila308@icloud.com>
2025-09-22 00:41:38 +0300

    +большая медведица

---
+++ constellation.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    puts("       *____* ");
+    puts("             \\");
+    puts("              \\");
+    puts("               *");
+    puts("                \\");
+    puts("                 \\");
+    puts("                  \\");
+    puts("                   *_______________*");
+    puts("                   |              / ");
+    puts("                   |             / ");
+    puts("                   *____________* ");
+    return 0;
+}
